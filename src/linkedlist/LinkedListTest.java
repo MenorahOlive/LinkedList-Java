a4837f3c664ad3c36ea60783b2e83a22c7be8741
Olive Menorah <you@example.com>
2023-10-23 01:07:42 +0300

    java project

---
+++ src/linkedlist/LinkedListTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author olive
+ */
+public class LinkedListTest {
+    
+     public static void main(String[] args) {
+      
+    }
+}
